6ebcca476541d1eb2451e4b5f1000d36068ea12b
Brock Luker <brockluker@gmail.com>
2013-10-28 16:13:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandp::Application.config.secret_token = '7783a2fb77be768c6eae137727f75e4a225134178e538fc192dc4ebb32984a02893ef07423bc050b754f3db35edc58ad90f3614028044d49c4f77d95a3144531'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brandp::Application.config.session_store :cookie_store, key: '_brandp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brandp::Application.config.session_store :active_record_store
